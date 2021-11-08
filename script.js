03b2e46c470ac195bb92148a11d98b68afb66603
Claudinei <claudineicampos95@gmail.com>
2021-11-08 12:30:03 -0300

    Finish the extension

--- script.js
+++ script.js
@@ -11,14 +11,6 @@ if (leadsFromLocalStorage) {
     render(myLeads)
 }
 
-tabBtn.addEventListener("click", function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        render(myLeads)
-    })
-})
-
 function render(leads) { 
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
@@ -40,11 +32,24 @@ inputBtn.addEventListener("click", function() {
     render(myLeads)
 })
 
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear()
     myLeads = []
     render(myLeads)
 })
+
+
+
+
+
 
 
 
